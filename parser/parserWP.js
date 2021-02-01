bde95e74b47dbd96c59ce60d7b1458b077ef7be6
Aleksey Lalysho <aleksey.lalysho@gns-it.com>
2021-02-01 10:59:50 +0200

    0.27 update date

--- parser/parserWP.js
+++ parser/parserWP.js
@@ -23,7 +23,7 @@ class parserWP {
   constructor () {
     this.limit = 30
     this.offset = 0
-    this.lastDate = new Date('2021-01-26T09:43:12.000Z') //DK
+    this.lastDate = new Date('2021-01-29T13:12:29.000Z') //DK
     // this.firstDate = new Date('2020-08-21T07:38:59.000Z') //DK
     // this.firstDate = new Date('2020-08-13T13:47:04.000Z') //DK
     this.firstDate = new Date('2007-09-15 12:11:07.000Z') //DK
