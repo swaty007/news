3c178b95fdc10b47586afb7148271f1655486447
swaty <swaty0007@gmail.com>
2021-04-16 10:39:33 +0300

    v0.38 update date

--- parser/parserWP.js
+++ parser/parserWP.js
@@ -16,7 +16,7 @@ class parserWP {
   constructor () {
     this.limit = 30
     this.offset = 0
-    this.lastDate = new Date('2021-04-09T11:39:36.000Z') //DK
+    this.lastDate = new Date('2021-04-15T11:20:27.000Z') //DK
     // this.firstDate = new Date('2020-08-21T07:38:59.000Z') //DK
     // this.firstDate = new Date('2020-08-13T13:47:04.000Z') //DK
     this.firstDate = new Date('2007-09-15 12:11:07.000Z') //DK
